803355d3a9f4c6cbfb57f48696eb679252ad8d60
ldd <122708125@qq.com>
2017-05-17 18:13:22 +0800

    reactjs + typescript + router + redux + webpack  初始化

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from "react-router-dom";
+import { RouteComponentProps } from "@types/react-router";
+
+const BasicExample = () => (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">首页</Link></li>
+                <li><Link to="/about">关于</Link></li>
+                <li><Link to="/topics">主题列表</Link></li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
+        </div>
+    </Router>
+);
+
+const Home = () => (
+    <div>
+        <h2>首页</h2>
+    </div>
+);
+
+const About = () => (
+    <div>
+        <h2>关于</h2>
+    </div>
+);
+
+const Topics = ({ match }: RouteComponentProps<any>) => (
+    <div>
+        <h2>主题列表</h2>
+        <ul>
+            <li>
+                <Link to={`${match.url}/rendering`}>
+                    Rendering with React
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/components`}>
+                    Components
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/props-v-state`}>
+                    Props v. State
+                </Link>
+            </li>
+        </ul>
+
+        <Route path={`${match.url}/:topicId`} component={Topic} />
+        <Route exact path={match.url} render={() => (
+            <h3>请选择一个主题</h3>
+        )} />
+    </div>
+);
+
+const Topic = ({ match }: RouteComponentProps<any>) => (
+    <div>
+        <h3>{match.params.topicId}</h3>
+    </div>
+);
+
+ReactDOM.render(<BasicExample />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const { resolve } = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: [
+        "webpack-dev-server/client?http://localhost:5000",
+        "./src/App.tsx"
+    ],
+    output: {
+        filename: "bundle.js",
+        path: resolve(__dirname, "dist"),
+        publicPath: "/"
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+    devServer: {
+        port: 5000,
+        publicPath: "/",
+        historyApiFallback: true,
+        hot: true,
+        inline: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ]
+}
